e1915c4778bb6c799a73debde3b0bd6a1ad78c41
HaXr <haxr@outlook.dk>
2023-03-23 20:40:37 +0100

    Fixed issue when respawn location is set to a "Respawn Anchor" block
    Also added some simple messages that lets the player know why the teleportation failed.

--- src/main/java/com/multibana/recallpotion/RecallEffect.java
+++ src/main/java/com/multibana/recallpotion/RecallEffect.java
@@ -1,6 +1,8 @@
 package com.multibana.recallpotion;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.RespawnAnchorBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
@@ -11,13 +13,16 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.property.Properties;
 import net.minecraft.tag.BlockTags;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.UseAction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
+import org.apache.commons.compress.compressors.lz77support.LZ77Compressor;
 import org.apache.logging.log4j.core.jmx.Server;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
@@ -31,6 +36,11 @@ public class RecallEffect extends StatusEffect {
         super(statusEffectCategory, color);
     }
 
+    void failedTeleport(LivingEntity target, ServerPlayerEntity player){
+        Vec3d pos = target.getPos();
+        target.world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_REDSTONE_TORCH_BURNOUT, SoundCategory.PLAYERS, 1f, 1f);
+    }
+
     // This method teleports a LivingEntity target to the spawn point of a ServerPlayerEntity player
     void teleportTargetToPlayerSpawn(LivingEntity target, ServerPlayerEntity player){
         BlockPos spawn = player.getSpawnPointPosition(); // Get the spawn point position of the player
@@ -39,17 +49,19 @@ public class RecallEffect extends StatusEffect {
         ServerWorld destination = ((ServerWorld) player.world).getServer().getWorld(spawnDimension); // Get the ServerWorld object of the spawn dimension
 
         // If the destination is null or in a different dimension than the player's spawn dimension, fail and play a sound at the target's position
-        if (destination == null || !(spawnDimension.equals(serverWorld.getRegistryKey()))) {
-            Vec3d pos = target.getPos();
-            target.world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_REDSTONE_TORCH_BURNOUT, SoundCategory.PLAYERS, 1f, 1f);
+        System.out.println("[RecallPotion] Printing destination: " + destination);
+        BlockState blockState = destination.getBlockState(spawn);
+        if (destination == null || !(spawnDimension.equals(serverWorld.getRegistryKey())) && blockState.isIn(BlockTags.BEDS)) {
+            failedTeleport(target, player);
+            player.sendMessage(Text.of("The 'Potion of Recall' is not powerful enough to teleport across dimensions!"), false);
             return;
         }
 
         // If the player doesn't have a spawn point, use the world spawn point instead
         if (spawn == null) {
-            Vec3d pos = target.getPos();
-            target.world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_REDSTONE_TORCH_BURNOUT, SoundCategory.PLAYERS, 1f, 1f);
-            player.sendMessage(Text.of("s"), true);
+            //spawn = ((ServerWorld) player.world).getSpawnPos();
+            failedTeleport(target, player);
+            player.sendMessage(Text.of("You do not have a home bed!"), false);
             return;
         }
         // Find a suitable respawn position for the target in the destination world
@@ -57,24 +69,31 @@ public class RecallEffect extends StatusEffect {
 
         // If a suitable respawn position is found, set the spawn point to that position
         if(a.isPresent()){
-            BlockState blockState = destination.getBlockState(spawn);
+            //BlockState blockState = destination.getBlockState(spawn);
+
             if(blockState.isIn(BlockTags.BEDS)){
                 spawn = new BlockPos(a.get());
                 System.out.println("[RecallPotion] {if(blockState.isIn(BlockTags.BEDS)} Setting player spawn location to: " + spawn);
             }
+            else if(!blockState.isIn(BlockTags.BEDS)){
+                failedTeleport(target, player);
+                player.sendMessage(Text.of("You do not have a home bed!"), false);
+                System.out.println("[RecallPotion] You have no home bed or charged respawn anchor, or it was obstructed: " + destination);
+                return;
+            }
             // If no suitable respawn position is found, try using the world spawn point instead
             else{
-                Optional<Vec3d> b = PlayerEntity.findRespawnPosition(destination, ((ServerWorld) player.world).getSpawnPos(), 0, true, true);
-                spawn = b.map(BlockPos::new).orElseGet(() -> ((ServerWorld) player.world).getSpawnPos());
-                System.out.println("[RecallPotion] {else} Setting player spawn location to: " + spawn);
+                //Optional<Vec3d> b = PlayerEntity.findRespawnPosition(destination, ((ServerWorld) player.world).getSpawnPos(), 0, true, true);
+                //spawn = b.map(BlockPos::new).orElseGet(() -> ((ServerWorld) player.world).getSpawnPos());
+                //System.out.println("[RecallPotion] {else} Setting player spawn location to: " + spawn);
 
                 // If the target is the same as the player, set the spawn point to the new respawn position
-                if (target.isPlayer()){
-                    ServerPlayerEntity tPlayer = (ServerPlayerEntity) target;
-                    if(tPlayer.equals(player)){
+                //if (target.isPlayer()){
+                   // ServerPlayerEntity tPlayer = (ServerPlayerEntity) target;
+                   // if(tPlayer.equals(player)){
                         //player.setSpawnPoint(serverWorld.getRegistryKey(), spawn, 0, true, false);
-                    }
-                }
+                    //}
+                //}
             }
         }
 
